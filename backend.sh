2be706356bb56eaae8c8d124c6d0ad55f6f31120
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-25 13:26:58 +0530

    ...

--- backend.sh
+++ backend.sh
@@ -58,6 +58,19 @@ else
     echo -e "expense user already exists.... $Y Skipping $N"
 fi
 
+mkdir -p /app
+VALIDATE $? "Creating /app folder..."
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOG_FILE
+VALIDATE $? "Downloading Backend Application code..."
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting Backend Application code..."
+
+
+
+
 
 
 
